94e5ee8f09ac53bd3b2db0ad334a8ee4c2a054bd
Amos Chen <amoschen@HackBook.local>
2017-05-07 20:31:27 -0700

    add test for .map that passes parameters into and calls each function in an array of anonymous functions

--- test/map.test.js
+++ test/map.test.js
@@ -6,4 +6,14 @@ describe('map()', () => {
     const mappedArr = _.map(arr, (el) => el * el);
     expect(mappedArr).toEqual([1, 4, 9, 16, 25]);
   });
+  it('maps every function in an array to their output given parameters (3, 2)', () => {
+    const arr = [
+        function(a, b) { return a + b; },
+        function(a, b) { return a * b; },
+        function(a, b) { return a - b; },
+        function(a, b) { return a + ', ' + b}
+    ];
+    const mappedArr = _.map(arr, (fn) => fn(3, 2));
+    expect(mappedArr).toEqual([5, 6, 1, '3, 2']);
+  });
 });
