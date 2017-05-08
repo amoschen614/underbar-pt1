0e350247b8e8ff93a044a61d98785b99584581de
Amos Chen <amoschen@HackBook.local>
2017-05-07 23:18:35 -0700

    add tests to .uniq for handling array of strings and object with heterogeneous property values

--- test/uniq.test.js
+++ test/uniq.test.js
@@ -5,4 +5,26 @@ describe('uniq()', () => {
     const nums = [2, 4, 5, 5, 7, 4, 10, 2];
     expect(_.uniq(nums)).toEqual([2, 4, 5, 7, 10]);
   });
+
+  it('de-dups a list of strings', () => {
+    const strs = ['a', 'b', 'c', 'b', 'c', 'd'];
+    expect(_.uniq(strs)).toEqual(['a', 'b', 'c', 'd']);
+  });
+
+  it('de-dups a list of heterogeneous object properties', () => {
+    const obj = {
+      a : 'a',
+      b : 'b',
+      c : 'b',
+      d : '2',
+      e : true,
+      f : '2',
+      g : false,
+      h : [1, 2, 3],
+      i : [1, 2, 3],
+      j : false,
+      k : {}
+    };
+    expect(_.uniq(obj)).toEqual(['a', 'b', '2', true, false, [1, 2, 3], {}]);
+  });
 });
