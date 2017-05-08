95484f52b84fa746d341d39b2b5f349ac7e8e9ce
Amos Chen <amoschen@HackBook.local>
2017-05-07 17:32:35 -0700

    add tests to show different kinds of behavior of .last when array is of length 1

--- test/last.test.js
+++ test/last.test.js
@@ -12,4 +12,16 @@ describe('last()', () => {
   it('returns the whole array if you ask for more elements than it has', () => {
     expect(_.last(['a', 'b', 'c'], 5)).toEqual(['a', 'b', 'c']);
   });
+
+  it('returns an empty array with no elemens if asked for no elements', () => {
+    expect(_.last(['a', 'b', 'c'], 0)).toEqual([]);
+  });
+
+  it('returns an array with only one element if given an array of length 1 and asked for more elements than it has', () => {
+    expect(_.last(['a'], 2)).toEqual(['a']);
+  });
+
+  it('returns only an element when given an array of length 1 and asked for all its elements', () => {
+    expect(_.last(['a'], 1)).toEqual('a');
+  });
 });
