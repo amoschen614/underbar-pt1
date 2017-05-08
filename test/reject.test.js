6a12ae051c3c0b0d0838b86714beed7f1b8eb3a2
Amos Chen <amoschen@HackBook.local>
2017-05-07 22:56:32 -0700

    add tests for .reject to filter non prime numbers from array and remove null-value or function-value properties from an object

--- test/reject.test.js
+++ test/reject.test.js
@@ -18,4 +18,35 @@ describe('reject()', () => {
     const orderItems = _.reject(order, (value) => value === null);
     expect(orderItems).toEqual(['burger', 'ketchup', 'cookie']);
   });
+
+  it('rejects non-prime numbers from an array', () => {
+    let nums = [];
+    for (let num = 2; num < 100; num++) {
+      nums.push(num);
+    }
+
+    const primes = _.reject(nums, (num) => {
+      let isPrime = true;
+      for (let i = 2; i < num; i++) {
+        isPrime = isPrime && (num % i !== 0);
+      }
+      return !isPrime;
+    });
+    expect(primes).toEqual([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]);
+  });
+
+  it('rejects all methods or null values from an object', () => {
+    const order = {
+      entree: 'burger',
+      getEntree: function() { return this.entree; },
+      changeEntree: function(entree) { this.entree = entree; },
+      side: null,
+      condiment: 'ketchup',
+      drink: null,
+      dessert: 'cookie'
+    };
+
+    const relevantItems = _.reject(order, prop => typeof prop === 'function' || prop === null);
+    expect(relevantItems).toEqual(['burger', 'ketchup', 'cookie']);
+  });
 });
