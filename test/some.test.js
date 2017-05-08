673aa6d2c38613dcebd9779d930daca5d8eb44d7
Amos Chen <amoschen@HackBook.local>
2017-05-07 22:11:51 -0700

    add tests for .some to search object properties and non-numeric arrays entries

--- test/some.test.js
+++ test/some.test.js
@@ -11,4 +11,23 @@ describe('some()', () => {
     expect(_.some(nums, num => num % 2 === 1)).toBe(false);
   });
 
+  it('returns false if no object property value is truthy', () => {
+    const obj = {
+      1: 0,
+      2: false,
+      3: null
+    };
+    expect(_.some(obj, propVal => !!propVal)).toBe(false);
+  });
+
+  it('returns true if any element in an array is truthy', () => {
+    const arr = [
+      0,
+      false,
+      null,
+      function() { return true; }()
+    ];
+    expect(_.some(arr, el => !!el)).toBe(true);
+  });
+
 });
