18b6b8478c831bf2c70951b8ca3af8bf46847f15
Amos Chen <amoschen@HackBook.local>
2017-05-07 21:16:25 -0700

    add additional test for .pluck to select called object methods

--- test/pluck.test.js
+++ test/pluck.test.js
@@ -19,4 +19,13 @@ describe('pluck()', () => {
     const result = _.pluck(people, 'name');
     expect(result).toEqual(['Harriet', 'Lazarus', 'Bethany']);
   });
+  it('returns an array of return values for functions keyed as \'age\'', () => {
+    const people = [
+      { name: 'Harriet', age: function() { return 12; }() },
+      { name: 'Lazarus', age: function() { return 999; }() },
+      { name: 'Bethany', age: function() { return 14; }() }
+    ];
+    const result = _.pluck(people, 'age');
+    expect(result).toEqual([12, 999, 14]);
+  })
 });
